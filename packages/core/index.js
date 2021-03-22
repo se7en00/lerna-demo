ed1d93d54f95a59b8820c8daabd0c343271e9b6c
se7en00 <seven0_0@126.com>
2021-03-22 15:01:59 +0800

    add sub module

---
+++ packages/core/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2021-03-22 15:01:04
+ * @LastEditors: jian sun
+ * @LastEditTime: 2021-03-22 15:01:05
+ */
+const _ = require('lodash')
+module.exports = {
+    test () {
+      
+      console.log('core-te2222st111');
+      console.log('core-test111');
+    }
+}
