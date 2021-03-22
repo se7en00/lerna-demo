ac782b09c097e1a258946d84c2809f3873945bc0
se7en00 <seven0_0@126.com>
2021-03-22 16:09:53 +0800

    update core version

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,13 +1,12 @@
 /*
  * @Date: 2021-03-22 15:01:04
  * @LastEditors: jian sun
- * @LastEditTime: 2021-03-22 15:01:05
+ * @LastEditTime: 2021-03-22 16:09:36
  */
 const _ = require('lodash')
 module.exports = {
     test () {
       
-      console.log('core-te2222st111');
       console.log('core-test111');
     }
 }
